21a0fc9fe8dddacafc6630f52fdc89f549b2cb13
Zee Shin <zee2013234@gmail.com>
2023-04-18 13:58:49 +0900

    Experiment
    Experimenting with:
    
    a) appBar
    b) body
    c) bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [Icon(Icons.star), Icon(Icons.star), Icon(Icons.star)]),
-        bottomNavigationBar: BottomAppBar(),
+        appBar: AppBar(
+          title: Text("앱임"),
+          centerTitle: false,
+        ),
+        body: Text("안녕s"),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 100,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(Icons.phone),
+                  Icon(Icons.message),
+                  Icon(Icons.contact_page),
+                ]),
+          ),
+        ),
       ),
     );
   }
