69bc9f9afd124a925d97444a7d83b6f9e3eb6670
Zee Shin <zee2013234@gmail.com>
2023-04-19 13:46:41 +0900

    State Control
    Learned how to reflect the changes right away

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,54 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var numeric = [0, 0, 0];
+  var name = ['Kim', 'Hong', 'Pizza'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: ListView(children: const [
-          Row(children: [Icon(Icons.person), Text("홍길동")]),
-          Row(children: [Icon(Icons.person), Text("홍길동")]),
-          Row(children: [Icon(Icons.person), Text("홍길동")]),
-        ]),
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: () {
+            setState(() {
+              a++;
+            });
+            print(a);
+          },
+        ),
+        appBar: AppBar(
+          title: Text('연락처앱'),
+          centerTitle: false,
+        ),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (c, i) {
+              return ListTile(
+                  leading: Text(numeric[i].toString()),
+                  title: Text(name[i]),
+                  trailing: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          numeric[i]++;
+                        });
+                      },
+                      child: Text("Like")));
+            }),
         bottomNavigationBar: BottomBar(),
       ),
     );
